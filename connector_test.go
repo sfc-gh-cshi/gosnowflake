dcfcc33915d176bb3a73338452534316b0202655
Di Wu <di.wu@snowflake.com>
2020-10-22 16:32:05 -0700

    f

--- connector_test.go
+++ connector_test.go
@@ -29,7 +29,9 @@ func TestSnowflakeConnector(t *testing.T) {
 	mock := snowflakeMockDriver{conn: &conn}
 	createDSN("UTC")
 	config, err := ParseDSN(dsn)
-
+	if err != nil {
+		t.Fatalf("Failed to parse dsn %s", dsn)
+	}
 	connector := SnowflakeConnector{driver: mock, cfg: *config}
 	connection, err := connector.Connect(context.Background())
 	if err != nil {

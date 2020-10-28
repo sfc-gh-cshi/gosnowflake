31a909c2db89190578e77e7ea21b9697cc20f2c9
Di Wu <di.wu@snowflake.com>
2020-10-28 16:34:46 -0700

    fix test

--- driver_test.go
+++ driver_test.go
@@ -2389,17 +2389,11 @@ func TestPingInvalidHost(t *testing.T) {
 }
 
 func TestOpenWithConfig(t *testing.T) {
-	config := Config{
-		Account:      account,
-		User:         user,
-		Password:     pass,
-		Host: host,
-		Database: dbname,
-		Schema: schemaname,
-	}
-	err := fillMissingConfigParameters(&config)
+	dsn = fmt.Sprintf("%s:%s@%s/%s/%s", user, pass, host, dbname, schemaname)
+	config := Config{}
+	err := parseDSNParams(&config, dsn)
 	if err != nil {
-		t.Fatalf("failed to fill missing config parameters. config: %v, err: %v", config, err)
+		t.Fatalf("failed to parse dsn. dsn: %v, err: %v", dsn, err)
 	}
 	driver := SnowflakeDriver{}
 	db, err := driver.OpenWithConfig(context.Background(), config)
